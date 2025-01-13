f23585e0f4b7349f84f35fd21b356ef399ecb3b2
Tomas Koller <111533593+TKOLLER22@users.noreply.github.com>
2025-01-13 01:24:03 +0100

    Solved 1a

--- Day1/day1a.js
+++ Day1/day1a.js
@@ -1,22 +1,40 @@
 const fs = require("fs");
 
-const text = fs.readFileSync("data.txt", "utf8", (error, data) =>{
-  if(error){console.log(error)}
-});
+// Read the file synchronously
+const text = fs.readFileSync("data.txt", "utf8");
 
+// Split the text into lines and then further split by whitespace
 const splitArr = text.split("\n");
 const prepedArr = splitArr.flatMap(item => item.split(/\s+/));
-prepedArr.pop();
-console.log(prepedArr);
 
-for(var i = 0, i<prepedArr.length, i++){
-  if(i%2){
-    
+// Initialize arrays for even and odd indices
+const left = [];
+const right = [];
+
+// Split the array into two parts based on the index
+for (let i = 0; i < prepedArr.length; i++) {
+  if (i % 2) {
+    right.push(Number(prepedArr[i]));
+  } else {
+    left.push(Number(prepedArr[i]));
   }
 }
 
+// Sort both arrays
+left.sort();
+right.sort();
 
+console.log("LEFT ARR -->", left);
+console.log("RIGHT ARR -->", right);
 
+// Sum up the values in each array
+let count = 0;
 
+for(let j = 0; j < left.length; j++){
+  count += Math.abs(left[j] - right[j]);
+  result = Math.abs(left[j] - right[j]);
+  console.log(left[j], " - ", right[j], " = ", result);
+}
 
+console.log("Result:", count);
 
